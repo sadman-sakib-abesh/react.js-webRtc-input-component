00dae90f0905cb9580b0ad8dad2889eca0e1dbad
sadman-sakib-abesh <63991795+sadman-sakib-abesh@users.noreply.github.com>
2021-07-12 17:03:24 +0600

    Update Input.js

--- Input.js
+++ Input.js
@@ -1,27 +1,47 @@
-import React, { Component } from 'react';
 import {useRef,useEffect,useState} from 'react'
 const Input =(props)=>{
         const videoRef = useRef(null);
         const audioRef=useRef(null)
+        
         const [audio,setAudio]=useState(false)
         const [video,setVideo]=useState(false)
+        const [share,setShare]=useState(false)
         const[tracks,setTrack]=useState('')
         const[atracks,setAtrack]=useState('')
+        
 
-
-        props.audioObj(audioRef)
+useEffect(()=>{
+    props.audioObj(audioRef)
 
         props.videoObj(videoRef)
 
+
+})
+        
     
 const play=()=>{
 
     navigator.mediaDevices
-    .getUserMedia({video:true,audio:false})
+    .getUserMedia({video:{ facingMode: `${props.videoFace}`},audio:false})
     .then(stream => {
         let vid = videoRef.current;
       vid.srcObject = stream;
 setTrack(vid.srcObject.getTracks())
+
+       vid.play()
+    }).catch(err=>{
+console.log(err)
+    })
+    
+}
+const splay=()=>{
+
+    navigator.mediaDevices.getDisplayMedia({video:true,audio:false})
+    .then(stream => {
+
+        let vid = videoRef.current;
+      vid.srcObject = stream;
+
 
        vid.play()
     }).catch(err=>{
@@ -54,6 +74,7 @@ const stop=()=>{
       });
 }
 
+
 const Astop=()=>{
     atracks.forEach((track)=> {
         track.stop();
@@ -63,9 +84,9 @@ const Astop=()=>{
 
           return (
             <div id={props.id}>
-        {video?<i class='fas fa-video' id={props.videoId} onClick={()=>{stop();setVideo(false)}}></i>:<i class='fas fa-video-slash' id={props.videoSlashId} onClick={()=>{play();setVideo(true)}}></i>}
-               
-                {audio?<i class='fas fa-microphone'  id={props.audioId} onClick={()=>{Astop();setAudio(false)}}></i>:<i class='fas fa-microphone-slash'  id={props.audioSlashId} onClick={()=>{Aplay();setAudio(true)}}></i>}
+        {video?<i class='fas fa-video' id={props.videoId} onClick={()=>{stop();setVideo(false)}}></i>:<i class='fas fa-video-slash' id={props.videoSlashId} onClick={()=>{play();setVideo(true)}}></i>}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        {audio?<i class='fas fa-microphone'  id={props.audioId} onClick={()=>{Astop();setAudio(false)}}></i>:<i class='fas fa-microphone-slash'  id={props.audioSlashId} onClick={()=>{Aplay();setAudio(true)}}></i>}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <i class='fas fa-arrow-alt-circle-up'   id={props.screenId} onClick={()=>{splay();setShare(true)}}></i>
                
               </div>
           )
